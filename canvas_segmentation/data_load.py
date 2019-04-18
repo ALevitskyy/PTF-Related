77faea38300b133be1a2341d7277dcf35644301e
Ubuntu <ubuntu@ip-172-31-63-181.us-west-2.compute.internal>
2019-04-18 03:28:55 +0000

    Adding Data Loader

---
+++ canvas_segmentation/data_load.py
@@ -0,0 +1,27 @@
+import subprocess
+from config import dataset_url, split_file,\
+        val_prop,image_folder
+from glob import glob
+import random
+import json
+import os
+filename = dataset_url.split("/")[-1]
+if os.path.exists(filename):
+    raise Exception("file already loaded")
+else:
+    subprocess.call(["wget", dataset_url])
+if os.path.exists(image_folder):
+    raise Exception("image folder  already exists")
+else:
+    subprocess.call(["unzip", filename, "-d", image_folder])
+images = glob("./images/**/images/*.png",recursive=True)
+masks = glob("./images/**/masks/*.png",recursive=True)
+pairs = list(zip(images, masks))
+random.shuffle(pairs)
+split = int(len(pairs)*val_prop)
+validation_list = pairs[:split]
+train_list = pairs[split:]
+file_content = {"valid":validation_list,
+                "train":train_list}
+with open(split_file,"w") as file:
+    file.write(json.dumps(file_content))
